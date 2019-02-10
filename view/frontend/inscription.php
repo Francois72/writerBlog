f970aa46161c3204421a8dcbbf51efc7a9a069c5
François <francoisbergerot@orange.fr>
2019-02-10 19:35:16 +0100

    page d'inscription avec formulaire

---
+++ view/frontend/inscription.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Inscription</title>
+    </head>
+  
+
+    <body>
+    
+    <form action="inscription_post.php" method="post">
+    <p>
+        <label>Pseudo</label><input type ="text" name="user" value=""/><br />
+        <label>Mot de passe</label><input type ="password" name="pass" value=""/><br />
+        <label>Retapez votre mot de passe</label><input type ="password" name="pass2" value=""/><br />
+        <label>Adresse email</label><input type ="text" name="email" value=""/><br />          
+        <label></label><input type="submit" value="Valider" />
+    </p>
+    </form>
+
+   
+    </body>
+</html>
