871a5540440a62138d17b609e6ace30ecb3d9667
François <francoisbergerot@orange.fr>
2019-02-10 19:50:53 +0100

    affichage du menu connexion/inscription... +

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+$_SESSION['test'] = 'Kizako';
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,8 +10,38 @@
         <title><?= $title ?></title>
         <link href="public/css/style.css" rel="stylesheet" /> 
     </head>
+
+    <header>
+        <div class="para1">
+            <h2>Bienvenue <?php
+            if (isset($_GET['user']))
+            {
+                echo $_GET['user'];
+            }    
+            ?>
+            </h2>
+
+        </div>
+        <div class="para2">
+            <nav>
+                <ul> <?php 
+                    if (isset($_GET['user'])) //$_SESSION
+                    {
+                       echo '<li><a href="view/frontend/deconnexion.php">Deconnexion</a></li>'; 
+                    }
+                    else
+                    {
+                        echo '<li><a href="view/frontend/inscription.php">Inscription</a></li>'; 
+                        echo '<li><a href="view/frontend/connexion.php">Connexion</a></li>';
+                    }
+                    ?>         
+                </ul>
+            </nav>
+        </div>
+    </header>
         
-    <body>
+    <body>        
         <?= $content ?>
     </body>
+    
 </html>
