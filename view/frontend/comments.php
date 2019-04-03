70729b7fce6174b76591ad583cb46c7badb730ca
François <francoisbergerot@orange.fr>
2019-04-03 19:07:08 +0200

    Passage en MVC

--- view/frontend/comments.php
+++ view/frontend/comments.php
@@ -4,10 +4,16 @@
 
 
 <?php
-include ("model/Manager.php");
-include ("model/postManager.php");
-$db = dbConnect();
-$post = getPost($db);
+
+/*
+include ("model/Manager.php");// a retirer
+include ("model/postManager.php");//
+$db = dbConnect(); //
+$post = getPost($db);//
+*/
+
+
+
 $data = $post->fetch();
 
 ?>
@@ -15,30 +21,36 @@ $data = $post->fetch();
 <?php
 echo '<h2><strong>'.$data['title']. '</strong></h2>';
 echo $data['content'];
-$post->closeCursor();
+
 
 ?>
 </div>
 
 <?php
 
-
+echo '<h1>Commentaires</h1>';
 
 if (isset($_SESSION['user']))
 {	
+
 	echo '<form action="index.php?action=postcomment" method="post">';
-	echo '<input name="user_id" type="hidden" value="'.$_SESSION['user'].'"/></p>';
+	
+	echo '<input name="user_id" type="hidden" value="'.$_SESSION['id'].'"/></p>';
+	// en gros je dois recup l'id de user
+
 	echo '<input name="post" type="hidden" value="  '.$_GET['post'].'  ">';
 	echo '<textarea class="tinymce" name="comment"></textarea>';
 	echo '<input type="submit" id="send_button" value="Envoyer" />';
 	echo '</form>';
 }
 
-include ("model/commentManager.php");
-$post = getComments($db);
+
+//include ("model/commentManager.php");
+
+$post = getComments();
 
 
-echo '<h1>Commentaires</h1>';
+
 
 while ($data = $post->fetch())
 {	
