34c0ebe99977c34d1f60a80231419d289a3d0af9
François <francoisbergerot@orange.fr>
2019-04-10 21:31:09 +0200

    fonction pour éditer un post

--- model/postManager.php
+++ model/postManager.php
@@ -1,31 +1,39 @@
 <?php
-function getPosts($db)
-{
-	$posts =$db->query('SELECT id, title, content, creation_date FROM postswriter ORDER BY creation_date DESC ');
-	return $posts;
-}
-
-function getPost($db)
-{
-	$post=$db->prepare('SELECT id, title, content, creation_date FROM postswriter WHERE id=?');
-	$post->execute(array($_GET['post']));
-	return $post;
-}
-
-/*
+require_once("Manager.php");
 class PostManager extends Manager
 {
-	function getPosts($db)
+	public function getPosts()
 	{
+		$db = $this -> dbConnect();		
 		$posts =$db->query('SELECT id, title, content, creation_date FROM postswriter ORDER BY creation_date DESC ');
 		return $posts;
 	}
 
-function getPost($db)
+	public function getPosts2()
 	{
+		$db = $this -> dbConnect();		
+		$posts =$db->query('SELECT id, title, content, creation_date FROM postswriter ORDER BY creation_date');
+		return $posts;
+	}
+
+	public function getPost()
+	{
+		$db = $this -> dbConnect();		
 		$post=$db->prepare('SELECT id, title, content, creation_date FROM postswriter WHERE id=?');
 		$post->execute(array($_GET['post']));
 		return $post;
 	}
+
+
+	public function editPost()
+	{
+		$db = $this -> dbConnect();		
+		$post=$db->prepare('UPDATE postswriter SET title = ?, content = ? WHERE id=?');
+		$post->execute(array($_POST['title'], $_POST['content'], $_GET['post']));
+		return $post;
+	}
+
 }
-*/
+
+
+//penser à suppriemr les commentaires du post supprimer
