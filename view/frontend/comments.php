7797835819f0241f1c9965ab1af6e361a4c06bd5
François <francoisbergerot@orange.fr>
2019-04-13 10:26:27 +0200

    rajout d'un bouton de signalement

--- view/frontend/comments.php
+++ view/frontend/comments.php
@@ -32,31 +32,37 @@ echo '<h1>Commentaires</h1>';
 
 if (isset($_SESSION['user']))
 {	
+	?>
 
-	echo '<form action="index.php?action=postcomment" method="post">';
+	<form action="index.php?action=postcomment" method="post">
 	
-	echo '<input name="user_id" type="hidden" value="'.$_SESSION['id'].'"/></p>';
-	// en gros je dois recup l'id de user
+	<input name="user_id" type="hidden" value="<?php echo $_SESSION['id'] ?>"/></p>
 
-	echo '<input name="post" type="hidden" value="  '.$_GET['post'].'  ">';
-	echo '<textarea class="tinymce" name="comment"></textarea>';
-	echo '<input type="submit" id="send_button" value="Envoyer" />';
-	echo '</form>';
+
+	<input name="post" type="hidden" value="<?php echo $_GET['post'] ?>">
+	<textarea class="tinymce" name="comment"></textarea>
+	<input type="submit" id="send_button" value="Envoyer" />
+	</form>
+
+	<?php
 }
 
 
 //include ("model/commentManager.php");
+/*
+$CommentsManager = new CommentsManager();
+$post = $CommentsManager -> getComments();
+*/
+//$post = getComments();
 
-$post = getComments();
 
 
 
-
-while ($data = $post->fetch())
+while ($data = $comments->fetch())
 {	
 	//echo $data['comment'].'</br>';
 
-	echo '<p><strong>'.$data['user'].'</strong> ('.$data['creation_date2'].') <strong>'.$data['comment'].'</strong></p>';
+	echo '<p><strong>'.$data['user'].'</strong> ('.$data['creation_date2'].') <strong>'.$data['comment'].'</strong><button>Signaler</button></p>';	
 }
 
 $post->closeCursor();
