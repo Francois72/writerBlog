67e06611b428731c5e357b0f27a44cc4f3f378a2
François <francoisbergerot@orange.fr>
2019-04-22 19:31:22 +0200

    MVC

--- view/frontend/inscription.php
+++ view/frontend/inscription.php
@@ -13,12 +13,13 @@
 
      <h1>Inscription</h1>
     
-    <form action="index.php?action=inscriptionpost" method="post">  
+    <form action="index.php?action=inscriptionpost" method="post"> 
+     
   
-        <p><label>Pseudo</label></br><input type ="text" name="user" value="" size="40"/></p>
-          <p><label>Mot de passe</label></br><input type ="password" name="pass" value="" size="40"/></p>
-          <p><label>Retapez votre mot de passe</label></br><input type ="password" name="pass2" value="" size="40"/></p>
-          <p><label>Adresse email</label></br><input type ="text" name="email" value="" size="40"/></p>  
+        <p><label>Pseudo</label></br><input type ="text" name="user" value="" size="40" required/></p>
+          <p><label>Mot de passe</label></br><input type ="password" name="pass" value="" size="40" required/></p>
+          <p><label>Retapez votre mot de passe</label></br><input type ="password" name="pass2" value="" size="40" required/></p>
+          <p><label>Adresse email</label></br><input type ="text" name="email" value="" size="40" required/></p>  
           <p><label></label><input type="submit" value="Valider" id="send_button"/></p>
     
     </form>
