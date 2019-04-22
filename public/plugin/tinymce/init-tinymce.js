799ee0215c9e96b3b163b5e6cd0b9d93b3b90dea
François <francoisbergerot@orange.fr>
2019-04-22 19:27:52 +0200

    quelques testes

--- public/plugin/tinymce/init-tinymce.js
+++ public/plugin/tinymce/init-tinymce.js
@@ -5,15 +5,25 @@ tinymce.init({
 	//statusbar: false,
 	forced_root_block : false,
 
-    menu: {
-        file: {title: 'File', items: 'newdocument'},
-        edit: {title: 'Edit', items: 'undo redo | cut copy paste pastetext | selectall'},
-        insert: {title: 'Insert', items: 'link media | template hr'},
-        view: {title: 'View', items: 'visualaid'},
-        format: {title: 'Format', items: 'bold italic underline strikethrough superscript subscript | formats | removeformat'},
-        table: {title: 'Table', items: 'inserttable tableprops deletetable | cell row column'},
-        tools: {title: 'Tools', items: 'spellchecker code'}
-    }
+    //menubar: 'edit insert view format table tools help',
+    menubar: '',
+
+    removed_menuitems: 'removeformat',
+
+
+
+  menu: {
+    file: {title: 'File', items: 'newdocument'},
+    edit: {title: 'Edit', items: 'undo redo | cut copy paste pastetext | selectall'},
+    insert: {title: 'Insert', items: 'link media | template hr'},
+    view: {title: 'View', items: 'visualaid'},
+    format: {title: 'Format', items: 'bold italic underline strikethrough superscript subscript | formats | removeformat'},
+    table: {title: 'Table', items: 'inserttable tableprops deletetable | cell row column'},
+    tools: {title: 'Tools', items: 'spellchecker code'}
+  }
+   
+
+   
 
 
 
