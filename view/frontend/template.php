a1c80df97b195fcc3dba0da8b06420cae6cbf144
François <francoisbergerot@orange.fr>
2019-04-22 19:34:40 +0200

    MVC

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -26,24 +26,23 @@
         <div class="para2">
             <nav>
                 <ul> <?php
+
+                    if ((isset($_SESSION['rights'])) AND ($_SESSION['rights'] == '1'))
+                    {
+                        echo '<li><a href="index.php?action=admin">Admin (non actif)</a></li>';
+                    }
+
                 	echo '<li><a href="index.php">Accueil</a></li>';
                     if (isset($_SESSION['user']))
                     {
-                       //echo '<li><a href="view/frontend/deconnexion.php">Deconnexion</a></li>';
                         echo '<li><a href="index.php?action=deconnexion">Deconnexion</a></li>';
-                       //renvois à l'index qui sous condition doit renvoyer à la bonne page
                     }
                     else
                     {
-                        //echo '<li><a href="view/frontend/inscription.php">Inscription</a></li>';
                         echo '<li><a href="index.php?action=inscription">Inscription</a></li>';
-                        //renvois à l'index qui sous condition doit renvoyer à la bonne page
-                        //index.php?action=inscription
-                        //echo '<li><a href="view/frontend/connexion.php">Connexion</a></li>';
                         echo '<li><a href="index.php?action=connexion">Connexion</a></li>';
-                        //renvois à l'index qui sous condition doit renvoyer à la bonne page
                     }
-                    ?>         
+                    ?>        
                 </ul>
             </nav>
         </div>
