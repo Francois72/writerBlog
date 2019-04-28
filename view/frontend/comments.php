934a5ad392b577410985f520e3b6003decc180c2
François <francoisbergerot@orange.fr>
2019-04-28 10:26:51 +0200

    redirection en cas de signalement

--- view/frontend/comments.php
+++ view/frontend/comments.php
@@ -1,72 +1,40 @@
 <?php $title = 'Mon blog'; ?>
-
 <?php ob_start(); ?>
 
-
 <?php
-
-/*
-include ("model/Manager.php");// a retirer
-include ("model/postManager.php");//
-$db = dbConnect(); //
-$post = getPost($db);//
-*/
-
-
-
 $data = $post->fetch();
-
 ?>
+
 <div class= authorLastPost>
+
 <?php
 echo '<h2><strong>'.$data['title']. '</strong></h2>';
 echo $data['content'];
-
-
 ?>
 </div>
 
 <?php
-
 echo '<h1>Commentaires</h1>';
-
 if (isset($_SESSION['user']))
 {	
 	?>
 
-	<form action="index.php?action=postcomment" method="post">
-	
-	<input name="user_id" type="hidden" value="<?php echo $_SESSION['id'] ?>"/></p>
-
-
-	<input name="post" type="hidden" value="<?php echo $_GET['post'] ?>">
-	<textarea class="tinymce" name="comment"></textarea>
-	<input type="submit" id="send_button" value="Envoyer" />
+	<form action="index.php?action=postcomment" method="post">	
+		<input name="user_id" type="hidden" value="<?php echo $_SESSION['id'] ?>"/></p>
+		<input name="post" type="hidden" value="<?php echo $_GET['post'] ?>">
+		<textarea class="tinymce" name="comment"></textarea>
+		<input type="submit" id="send_button" value="Envoyer"/>
 	</form>
 
 	<?php
 }
 
-
-//include ("model/commentManager.php");
-/*
-$CommentsManager = new CommentsManager();
-$post = $CommentsManager -> getComments();
-*/
-//$post = getComments();
- 
-
-
-
 while ($data = $comments->fetch())
 {	
-	//echo $data['comment'].'</br>';
-
 	echo '<p><strong>'.$data['user'].'</strong> ('.$data['creation_date2'].') <strong>'.$data['comment'].'</strong>';
-
 	if (isset($_SESSION['user']))
-	{
-		echo '<button><a href=index.php?action=report&post='.$data['id'].'>Signaler</a></button></p>';
+	{		
+		echo '<button><a href=index.php?action=report&commentid='.$data['id'].'&postid='.$_GET['post'].'>Signaler</a></button></p>';
 	}	
 }
 
