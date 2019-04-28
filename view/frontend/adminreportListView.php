111a1dec11df28d8955f3eaa899f1f06ab4409a4
François <francoisbergerot@orange.fr>
2019-04-28 10:28:43 +0200

    Supprimer un commentaire signalé/ignorer le signalement

--- view/frontend/adminreportListView.php
+++ view/frontend/adminreportListView.php
@@ -1,18 +1,19 @@
 <?php $title = 'Mon blog'; ?>
-
 <?php ob_start(); ?>
 
-
 <?php
-
-
+echo 'Liste des commentaires signalés:</br>';
 while ($data = $post->fetch())
 {
-	echo $data['user'].' '. $data['comment'].'</br>';		
+	echo $data['user'].': '.$data['comment'];	
+	?>
+	<em><a href="index.php?action=ignore&post=<?php echo $data['id']?>">ignorer</a></em>
+	<em><a href="index.php?action=supprimercommentaire&post=<?php echo $data['id']?>">supprimer</a></em>
+	</br>
+<?php
 }
-/*2 boutons supprimer / ignorer*/
-
 ?>
+<em><a href="index.php?action=admin">Retour</a></em>
 
 <?php $content = ob_get_clean(); ?>
 <?php require('template.php'); ?>
