644092b8933db2ec32821ad7efce663aa7301759
François <francoisbergerot@orange.fr>
2019-05-11 22:06:33 +0200

    rajout d'un lien pour sortir de la page

--- view/frontend/connexion.php
+++ view/frontend/connexion.php
@@ -8,7 +8,8 @@
     
   
 
-    <body> 
+    <body>
+    <a href="index.php">retour</a>
 
     <h1>Connexion</h1>
       
@@ -17,10 +18,11 @@
     <!--<form action="view/frontend/connexion_post.php" method="post">-->
     <form action="index.php?action=connexionpost" method="post">
         <p><label>Pseudo: </label></br><input type ="text" name="user" value="" size="40" required/></p>
-        <p><label>Mot de passe: </label></br><input type ="password" name="pass" value="" size="40" required/></p> 
-        <p><label>Connexion automatique </label></br><input type ="checkbox" name="coAuto"/></p>         
+        <p><label>Mot de passe: </label></br><input type ="password" name="pass" value="" size="40" required/></p>                
         <p><label></label><input type="submit" id="send_button" value="Se connecter" /></p>    
     </form>
+
+
    
     </body>
 </html>
