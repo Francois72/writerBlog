feda8ce7a77a30cf5c9be73fdd23433d211baa03
François <francoisbergerot@orange.fr>
2019-05-30 21:01:20 +0200

    indentation

--- view/frontend/inscription.php
+++ view/frontend/inscription.php
@@ -3,28 +3,22 @@
     <head>
         <meta charset="utf-8" />
         <title>Inscription</title>
-        <link href="public/css/style2.css" rel="stylesheet" />   
-
-
+        <link href="public/css/style2.css" rel="stylesheet" />
     </head>
   
 
     <body>
-    <a href="index.php">retour</a>
+      <a href="index.php">retour</a>
 
-     <h1>Inscription</h1>
-    
-    <form action="index.php?action=inscriptionpost" method="post"> 
-     
-  
-        <p><label>Pseudo</label></br><input type ="text" name="user" value="" size="40" required/></p>
+       <h1>Inscription</h1>
+      
+      <form action="index.php?action=inscriptionpost" method="post">              
+          <p><label>Pseudo</label></br><input type ="text" name="user" value="" size="40" required/></p>
           <p><label>Mot de passe</label></br><input type ="password" name="pass" value="" size="40" required/></p>
           <p><label>Retapez votre mot de passe</label></br><input type ="password" name="pass2" value="" size="40" required/></p>
           <p><label>Adresse email</label></br><input type ="text" name="email" value="" size="40" required/></p>  
-          <p><label></label><input type="submit" value="Valider" id="send_button"/></p>
-    
-    </form>
-
-   
+          <p><label></label><input type="submit" value="Valider" id="send_button"/></p>      
+      </form>
+     
     </body>
 </html>
